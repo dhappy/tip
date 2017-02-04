d46d8e790446ef9367c031cae6e6b32a75fb23ca
Will Holcomb <will@dhappy.org>
2017-02-03 20:50:58 -0500

    cat

--- test.rb
+++ test.rb
@@ -4,5 +4,16 @@ require 'pry'
 require 'ipfs/client'
 
 cli = IPFS::Client.new host: 'http://ipfs.io', port: 80
-result = cli.ls 'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG'
-binding.pry
+results = cli.ls 'QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG'
+links = results.collect(&:links).flatten
+
+links.each do |entry|
+  puts entry.name
+end
+
+links.find{ |link| link.name == 'readme' }.tap do |readme|
+  content = cli.cat readme.hashcode
+  puts content
+end
+
+
