c9a3f8a13cb365c816f1bf02f1a4cd9a7d580bc6
Will Holcomb <will@dhappy.org>
2017-03-10 05:57:11 -0500

    anchors should work this way

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302230048) do
+ActiveRecord::Schema.define(version: 20170310104922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170302230048) do
     t.datetime "updated_at",  null: false
     t.string   "destination"
     t.string   "name"
+    t.integer  "size"
   end
 
   create_table "entries_spaces", id: false, force: :cascade do |t|
